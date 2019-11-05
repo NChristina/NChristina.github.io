8559d0bed3eb1376f6993e3b078a6fdf4ef00393
NChristina <Christina.Niederer@fhstp.ac.at>
2019-11-05 16:32:03 +0100

    Initial Commit

---
+++ precache-manifest.bfb3a77a925c238440853d7680b86940.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cb24708a7e3ef95c88dd",
+    "url": "/static/js/main.0289a839.chunk.js"
+  },
+  {
+    "revision": "155072995bbc6f54262b",
+    "url": "/static/js/2.09e807af.chunk.js"
+  },
+  {
+    "revision": "cb24708a7e3ef95c88dd",
+    "url": "/static/css/main.f987e6be.chunk.css"
+  },
+  {
+    "revision": "155072995bbc6f54262b",
+    "url": "/static/css/2.d1c01a99.chunk.css"
+  },
+  {
+    "revision": "55c7f562bdb5584d3db2c43922bf8c66",
+    "url": "/index.html"
+  }
+];
